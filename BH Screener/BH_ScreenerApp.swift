6dc38d97b5cb868c6b705e16321f4dad235e5832
Arthur Ford <adf.ford@gmail.com>
2022-12-20 11:16:03 -0500

    Initial Commit

---
+++ BH Screener/BH_ScreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BH_ScreenerApp.swift
+//  BH Screener
+//
+//  Created by Arthur Ford on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BH_ScreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
